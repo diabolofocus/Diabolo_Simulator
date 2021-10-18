9252f84aca1351d9eedcb2e3d0f4bc9e228a2b83
TysonJGrant <54131201+TysonJGrant@users.noreply.github.com>
2021-10-18 11:58:17 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width">
+  <meta charset="UTF-8">
+  <style>
+    label{
+      color: #333;
+      font-size: 10px;
+    font-family: sans-serif;
+    display: block;
+    font-weight: bold;
+    text-align: center;
+    }
+  </style>
+  <script language="javascript" type="text/javascript" src="p5.js"></script>
+  <script language="javascript" src="p5.dom.js"></script>
+  <script src="hammer.min.js"></script>
+  <script language="javascript" type="text/javascript" src="sketch.js"></script>
+  <script language="javascript" type="text/javascript" src="Diabolohigh.js"></script>
+  <script language="javascript" type="text/javascript" src="Diabololow.js"></script>
+
+  <title>Diabolo Simulator</title>
+  <meta property="og:description" content="
+    <?php
+        if (isset($_GET['link'])) {
+            echo "Try this siteswap: " + $_GET['ss'];
+        } else {
+            echo "Try the simulator!";
+        }
+    ?>
+    ">
+  <meta property="og:image" content="https://tysonjgrant.github.io/Diabolo_Simulator/assets/preview.jpg">
+</head>
+
+<body>
+  <div id="fb-root"></div>
+  <!-- <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v12.0" nonce="Xnj1zwkS"></script> -->
+  <div id="fbshare" class="fb-share-button" style="z-index:1; background-image: url(assets/fbshare.png);" data-href="https://tysonjgrant.github.io/Diabolo_Simulator" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftysonjgrant.github.io%2FDiabolo_Simulator&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div></body>
+</html>
